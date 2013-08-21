218b6c1519523e357b31e3b8a9863400add2d352
Jonathan <contact@jonathanschmidt.fr>
2013-08-21 10:23:15 +0200

    Update for js loader

--- main.go
+++ main.go
@@ -3,50 +3,68 @@ package main
 import (
 	"fmt"
 	"net/http"
-    "labix.org/v2/mgo"
+	"labix.org/v2/mgo"
 	"time"
+	"flag"
+	"text/template"
 )
-/* ********************************************
- *                   TYPES                    *
- * ********************************************/
-type Handler struct{}
 /* ********************************************
  *            VARIABLES GLOBALES              *
  * ********************************************/
 var collection string
 var database string
+
+var port = flag.String("p","9090","Listening port")
+var js = flag.String("js","both","Using or not JS Loader / HTTP Listener . ( no-js / no-go / both )")
 /* ********************************************
  *                FUNCTIONS                   *
  * ********************************************/
 
 func main() {
-	fmt.Println("Listening 9090")
-	http.ListenAndServe(":9090", new(Handler))
+	flag.Parse()
+	listenport := fmt.Sprint(":",*port)
+	fmt.Println("Listening ", listenport)
+
+	if *js != "no-go" {
+		http.HandleFunc("/tag", TagHandler)
+	}
+	if *js != "no-js" {
+		http.HandleFunc("/loader.js",LoaderHandler)
+	}
+
+	http.ListenAndServe(listenport,nil)
 }
 
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func TagHandler(w http.ResponseWriter, r *http.Request) {
 	m := make( map [string][]string)
 	m = r.URL.Query()
 	go Insertdata(m)
 	return
 }
 
+func LoaderHandler (w http.ResponseWriter, r *http.Request) {
+	loader, _ :=  template.ParseFiles("templates/loader.tmpl")
+	data := make(map[string]interface{})
+	data["domain"] = "jonathanschmidt.fr:9090"
+	data["name"] = "visite"
+	loader.Execute(w,data)
+}
 
 func Insertdata(m map[string][]string) {
 	database := "gotest"
 	collection := "visite"
-    session, err := mgo.Dial("127.0.0.1")
-    if err != nil {
-        panic(err)
-    }
-    defer session.Close()
-    c := session.DB(database).C(collection)
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB(database).C(collection)
 	if len(m)!=0 {
 		m["insert_date"] = []string {time.Now().Format("2006-01-02 15:04:05")}
 		m["source"] = []string {"GoTag"}
 		err = c.Insert(m)
 		if err != nil {
-	        panic(err)
+			panic(err)
 		}
 	}
 }
