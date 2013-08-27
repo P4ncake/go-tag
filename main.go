f9d7f63467c520ef939b952f7611281834513b65
Jonathan <contact@jonathanschmidt.fr>
2013-08-27 16:36:42 +0200

    confg file handle

--- main.go
+++ main.go
@@ -1,54 +1,80 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"labix.org/v2/mgo"
-	"time"
 	"flag"
+	"fmt"
+	"labix.org/v2/mgo"
+	"net/http"
 	"text/template"
+	"time"
+	"github.com/sbinet/go-config/config"
 )
+
 /* ********************************************
  *            VARIABLES GLOBALES              *
  * ********************************************/
 var collection string
 var database string
+var baseUrl string
+var loaderName string
+var loaderUrl string
+var tagName string
+var tagUrl string
+var port string
+//var port = flag.String("p", "9090", "Listening port")
+var js = flag.String("js", "both", "Using or not JS Loader / HTTP Listener . ( no-js / no-go / both )")
 
-var port = flag.String("p","9090","Listening port")
-var js = flag.String("js","both","Using or not JS Loader / HTTP Listener . ( no-js / no-go / both )")
 /* ********************************************
  *                FUNCTIONS                   *
  * ********************************************/
 
 func main() {
+
+	Getconf()
+
 	flag.Parse()
-	listenport := fmt.Sprint(":",*port)
+	listenport := fmt.Sprint(":", port)
 	fmt.Println("Listening ", listenport)
 
-	if *js != "no-go" {
-		http.HandleFunc("/tag", TagHandler)
+	if tagUrl != "" {
+		http.HandleFunc(tagUrl, TagHandler)
 	}
-	if *js != "no-js" {
-		http.HandleFunc("/loader.js",LoaderHandler)
+	if loaderUrl != "" {
+		http.HandleFunc(loaderUrl, LoaderHandler)
 	}
 
-	http.ListenAndServe(listenport,nil)
+	http.ListenAndServe(listenport, nil)
 }
 
+func Getconf() {
+
+	// Reading configuration file
+	c, _ := config.ReadDefault("go-tag.cfg")
+
+	// Get configuration variables
+	baseUrl, _ = c.String("default","base-url")
+	port, _ = c.String("default","port")
+	loaderName, _ = c.String("default","loader-name")
+	loaderUrl, _  = c.String("default","loader-url")
+	tagName, _ = c.String("default","tag-name")
+	tagUrl, _ = c.String("default","tag-url")
+}
+
+
 func TagHandler(w http.ResponseWriter, r *http.Request) {
-	m := make( map [string][]string)
+	m := make(map[string][]string)
 	m = r.URL.Query()
 	go Insertdata(m)
 	return
 }
 
-func LoaderHandler (w http.ResponseWriter, r *http.Request) {
-	loader, _ :=  template.ParseFiles("templates/loader.tmpl")
+func LoaderHandler(w http.ResponseWriter, r *http.Request) {
+	loader, _ := template.ParseFiles("templates/loader.tmpl")
 	data := make(map[string]interface{})
 	data["domain"] = "jonathanschmidt.fr:9090"
 	data["name"] = "visite"
 	data["prefix"] = "gotag_"
-	loader.Execute(w,data)
+	loader.Execute(w, data)
 }
 
 func Insertdata(m map[string][]string) {
@@ -60,9 +86,9 @@ func Insertdata(m map[string][]string) {
 	}
 	defer session.Close()
 	c := session.DB(database).C(collection)
-	if len(m)!=0 {
-		m["insert_date"] = []string {time.Now().Format("2006-01-02 15:04:05")}
-		m["source"] = []string {"GoTag"}
+	if len(m) != 0 {
+		m["insert_date"] = []string{time.Now().Format("2006-01-02 15:04:05")}
+		m["source"] = []string{"GoTag"}
 		err = c.Insert(m)
 		if err != nil {
 			panic(err)
